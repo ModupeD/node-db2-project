9a8c429f4121d06dc190caf56b39ec77ec646d36
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2021-12-09 07:56:02 +0100

    started

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,33 @@
 // STRETCH
+const cars = [
+{
+ vin: '11111111111111',
+ make: 'toyota',
+ model: 'prius',
+ mileage: 215000,
+ title: 'clean',
+ transmission: 'manual',
+},
+{
+    vin: '11111111111111',
+    make: 'totyota',
+    model: 'corolla',
+    mileage: 115000,
+    title: 'salvage',
+   },
+   {
+    vin: '11111111111111',
+    make: 'ford',
+    model: 'focus',
+    mileage: 15000,
+    title: 'clean',
+    
+   },
+]
+
+exports.seed = async function(knex) {
+   
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+    
+}
