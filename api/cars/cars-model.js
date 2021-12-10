ab49c22b87ffcec9cf78c5a71b2b3a05a341ccfa
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2021-12-10 12:53:08 +0100

    working

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,20 +1,28 @@
-const db = require('../../data/db-config')
+const db = require("../../data/db-config");
 
 const getAll = () => {
-  return db('cars')
-}
+  return db("cars");
+};
 
 const getById = (id) => {
-  return db('cars').where('id',id).first()
-}
+  return db("cars").where("id", id).first();
+};
 
-const create = () => {
-  // DO YOUR MAGIC
-}
+const getByVin = (vin) => {
+  return db("cars").where("vin", vin).first();
+};
 
+const create = (car) => {
+  return db("cars")
+    .insert(car)
+    .then(([id]) => {
+      return getById(id);
+    });
+};
 
 module.exports = {
   getAll,
   getById,
   create,
-}
+  getByVin,
+};
